2e3717f6f898b66e4c1b8baa041d34be1f80013f
Top丶齊 <1970390409@qq.com>
2020-05-26 19:57:51 +0800

    287：寻找重复数

---
+++ find_duplicate.cpp
@@ -0,0 +1,30 @@
+//寻找重复数
+
+/*
+    给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。
+    假设只有一个重复的整数，找出这个重复的数。
+
+    说明：
+    不能更改原数组（假设数组是只读的）。
+    只能使用额外的 O(1) 的空间。
+    时间复杂度小于 O(n2) 。
+    数组中只有一个重复的数字，但它可能不止重复出现一次。
+*/
+
+//flod算法
+int findDuplicate(vector<int> &nums)
+{
+    int slow = 0, fast = 0;
+    do
+    {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast);
+    slow = 0;
+    while (slow != fast)
+    {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return slow;
+}
